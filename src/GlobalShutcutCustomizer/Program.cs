8752bfcbc4351f856aefc85a95f15a47ae8625fc
nanagami1369 <nanagami1369@gmail.com>
2021-02-21 00:46:27 +0900

    設定ファイルを読み込む処理を追加

--- src/GlobalShutcutCustomizer/Program.cs
+++ src/GlobalShutcutCustomizer/Program.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
+using ExcelDataReader;
 
 namespace GlobalShutcutCustomizer
 {
@@ -13,6 +16,34 @@ namespace GlobalShutcutCustomizer
         {
             var body = new AppBody();
             Application.Run();
+        }
+        public static Setting[] ReadSetting(string settingPath)
+        {
+            using (var stream = File.Open(settingPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                // ヘッダーを読み飛ばす
+                reader.Read();
+
+                var settings = new List<Setting>();
+                while (reader.Read())
+                {
+                    var name = reader.GetString(0);
+                    var args = reader.GetString(1) ?? "";
+                    var keyString = reader.GetString(2);
+                    var key = SettingUtil.StringToKeys(keyString);
+                    var path = reader.GetString(3);
+                    var setting = new Setting()
+                    {
+                        Name = name,
+                        Args = args,
+                        Key = key,
+                        Path = path
+                    };
+                    settings.Add(setting);
+                }
+                return settings.ToArray();
+            }
         }
     }
 }
