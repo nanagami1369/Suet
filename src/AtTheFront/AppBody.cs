aabb8d32c0f0bbc6253c7cc8a804456826747718
nanagami1369 <nanagami1369@gmail.com>
2021-06-30 15:43:31 +0900

    常駐できるようにした

---
+++ src/AtTheFront/AppBody.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using NHotkey.WindowsForms;
+
+namespace AtTheFront
+{
+    public class AppBody : Form
+    {
+        public AppBody(string option, Action execMethod)
+        {
+            ShowInTaskbar = false;
+            SetComponents();
+            try
+            {
+                var keys = SettingUtil.StringToKeys(option);
+                HotkeyManager.Current.AddOrReplace(GetAppName(), keys, (s, e) => execMethod());
+            }
+            catch (FormatException e)
+            {
+                MessageBox.Show(e.Message);
+                Environment.Exit(-1);
+            }
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SetComponents()
+        {
+            var icon = new NotifyIcon();
+            icon.Icon = GetAppIcon();
+            icon.Visible = true;
+            icon.Text = GetAppName();
+            var menu = new ContextMenuStrip();
+            var menuItem = new ToolStripMenuItem();
+            menuItem.Text = "&終了";
+            menuItem.Click += Close_Click;
+            menu.Items.Add(menuItem);
+            icon.ContextMenuStrip = menu;
+        }
+
+        private Icon GetAppIcon()
+        {
+            var appDirPath = $"{AppDomain.CurrentDomain.BaseDirectory}";
+            var appName = GetAppName();
+            var appPath = Path.Combine(appDirPath, appName);
+            return Icon.ExtractAssociatedIcon(appPath);
+        }
+
+        private string GetAppName()
+        {
+            return Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+        }
+    }
+}
--- src/AtTheFront/Program.cs
+++ src/AtTheFront/Program.cs
@@ -1,4 +1,9 @@
 using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using NHotkey;
+using NHotkey.WindowsForms;
 
 namespace AtTheFront
 {
@@ -7,7 +12,47 @@ namespace AtTheFront
         [STAThread]
         private static void Main(string[] args)
         {
-            ToFront();
+            // スタンドアローンモード
+            if (args.Length == 2 && (args[0] == "-s" || args[0] == "/s"))
+            {
+                try
+                {
+                    var app = new AppBody(args[1], ToFront);
+                    Application.ThreadException += (sender, e) =>
+                    {
+                        MessageBox.Show(e.Exception.Message);
+                        var appName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+                        HotkeyManager.Current.Remove(appName);
+                    };
+                    Thread.GetDomain().UnhandledException += (sender, e) =>
+                    {
+                        if (e.ExceptionObject is Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            var appName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+                            HotkeyManager.Current.Remove(appName);
+                        }
+                    };
+                    Application.Run();
+                }
+                catch (HotkeyAlreadyRegisteredException e)
+                {
+                    MessageBox.Show(e.Message);
+                    var appName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+                    HotkeyManager.Current.Remove(appName);
+                    return;
+                }
+                finally
+                {
+                    var appName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+                    HotkeyManager.Current.Remove(appName);
+                }
+                return;
+            }
+            else
+            {
+                ToFront();
+            }
         }
 
         public static void ToFront()
