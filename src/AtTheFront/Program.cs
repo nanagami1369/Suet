a7d39634f5e40eb3faa9b64f76ae9824e2555a69
nanagami1369 <nanagami1369@gmail.com>
2021-06-30 15:45:53 +0900

    ヘルプの作成

--- src/AtTheFront/Program.cs
+++ src/AtTheFront/Program.cs
@@ -12,6 +12,29 @@ namespace AtTheFront
         [STAThread]
         private static void Main(string[] args)
         {
+            // ヘルプ
+            if (args.Length >= 1 && (args[0] == "/?" || args[0] == "-h" || args[0] == "--help"))
+            {
+                MessageBox.Show(
+                    $@"使用法:
+    {Path.GetFileName(Environment.GetCommandLineArgs()[0])} -s [Key]
+フォーカスのあたっているウィンドウを最前面に固定します
+すでにウィンドウが最前面に固定されている場合は解除します
+
+オプション:
+    /? -h --help ヘルプ
+    -s /s [Key]スタンドアローンモード アプリ自体が常駐して
+    [key]に指定したキーが入力された場合に最前面に表示します
+
+例:
+    {Path.GetFileName(Environment.GetCommandLineArgs()[0])}                 ...実行
+    {Path.GetFileName(Environment.GetCommandLineArgs()[0])} -s Ctrl+Shift+K ...Ctrl+Shift+Kを同時に押すと実行
+    {Path.GetFileName(Environment.GetCommandLineArgs()[0])} -s Ctrl+Alt+S   ...Ctrl+Alt+Sを同時に押すと実行
+    {Path.GetFileName(Environment.GetCommandLineArgs()[0])} -s Shift+F11    ...Shift+F11を同時に押すと実行
+");
+                return;
+            }
+
             // スタンドアローンモード
             if (args.Length == 2 && (args[0] == "-s" || args[0] == "/s"))
             {
